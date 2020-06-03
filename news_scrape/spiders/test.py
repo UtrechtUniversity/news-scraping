38dc8f0b95d8a95ebba734bfa7f7cfc27263f79e
Shiva Nadi <s.nadi@uu.nl>
2020-06-03 08:39:41 +0200

    add item to test

--- news_scrape/spiders/test.py
+++ news_scrape/spiders/test.py
@@ -1,25 +1,43 @@
 import scrapy
 import re
 import datetime
-
+from ..items import TutorialItem
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-#
-# TAG_RE = re.compile(r'<[^>]+>')
-#
-#
-# def remove_tags(text):
-#     return TAG_RE.sub('', text)
+
 
 class NewsSpider(scrapy.Spider):
     name = 'news'
 
     start_urls = ['https://www.geenstijl.nl/']
 
+    # function to remove html tags, used for cleaning footer
     TAG_RE = re.compile(r'<[^>]+>')
-
     def remove_tags(self,text):
         return self.TAG_RE.sub('', text)
+
+    #function for removing extra \n and spaces, used in cleaning footer
+    def clean(self, line):
+        line = line.strip()
+        line = re.sub("\n", "", line)
+        line = re.sub("\xa0|", "", line)
+        line = re.sub(" ", "", line)
+        line = re.sub(",", "", line)
+        return line
+
+    # function for extracting time and date from footer
+    import re
+    def date_func(self, text):
+        date = re.findall(r'[0-9]{2}[-|\/]{1}[0-9]{2}[-|\/]{1}[0-9]{2}', text)
+        return date
+
+    def time_func(self, text):
+        time = re.findall(r'(?:[01]\d|2[0123]):(?:[012345]\d)', text)
+        return time
+
+
+
+
 
     def parse(self, response):
         author_page_linkss = response.xpath('//h1/a/@href')
@@ -34,21 +52,61 @@ class NewsSpider(scrapy.Spider):
             return response.xpath(query).get()
             # return response.xpath(query).get(default='').strip()
 
-        yield {
-            'title': extract_with_xpath("//div[@class='col-xs-12']/h1/text()"),
-            'article_info': extract_with_xpath("//div[@class='article-intro']/p/text()"),
-            # 'article_body': self.remove_tags(extract_with_xpath("//div[@class='article_content']/p")),
-            'article_body': response.xpath("//div[@class='article_content']/p/text()").getall(),
-            'footer': self.remove_tags(extract_with_xpath("//div[@class='art-footer']/div[@class='col-xs-12 col-sm-7']")),
-            'created_at': datetime.datetime.now(),
-            'image': response.xpath("//div[@class='article_content']/*/img/@src").getall(),
-            'reactions': extract_with_xpath("//div[@class='col-xs-12 col-sm-7']/a[@id='comment-count']/text()"),
-            'author': extract_with_xpath("//div[@class='col-xs-12 col-sm-7']/a[1]/text()"),
-            'doctype': 'geenstijl.nl',
-            'url': response.url,
-            'tags': response.xpath("//ul[@class='art-tags']/li/a/text()").getall(),
-            'twitter': extract_with_xpath("//div[@class='icon-twitter pull-left']/a/@href"),
-            'facebook':extract_with_xpath("//div[@class='icon-facebook pull-left']/a/@href"),
-            'iframe': response.xpath("//iframe[@class='puu-video_frame']/@data-src").get(),
+        title = extract_with_xpath("//div[@class='col-xs-12']/h1/text()")
 
-        }
+        article_info = extract_with_xpath("//div[@class='article-intro']/p/text()")
+
+        article_body= response.xpath("//div[@class='article_content']/p//text()").getall()
+        article_body_str = ''.join(str(e) for e in article_body)
+
+        footer= self.remove_tags(extract_with_xpath("//div[@class='art-footer']/div[@class='col-xs-12 col-sm-7']"))
+        footer_clean = self.clean(footer)
+        date = self.date_func(footer_clean)
+        publication_date = ''.join(str(e) for e in date)
+        time = self.time_func(footer_clean)
+        publication_time= ''.join(str(e) for e in time)
+
+        created_at = datetime.datetime.now()
+        image = response.xpath("//div[@class='article_content']/*/img/@src").getall()
+        reactions = extract_with_xpath("//div[@class='col-xs-12 col-sm-7']/a[@id='comment-count']/text()")
+        author = extract_with_xpath("//div[@class='col-xs-12 col-sm-7']/a[1]/text()")
+        doctype = 'geenstijl.nl'
+        url = response.url
+        tags_list = response.xpath("//ul[@class='art-tags']/li/a/text()").getall()
+        tags =', '.join(str(i) for i in tags_list)
+
+        twitter = extract_with_xpath("//div[@class='icon-twitter pull-left']/a/@href")
+        facebook = extract_with_xpath("//div[@class='icon-facebook pull-left']/a/@href")
+        iframe = response.xpath("//iframe[@class='puu-video_frame']/@data-src").get()
+        #
+        items = TutorialItem()
+        items['title'] = title
+        items['article_info'] = article_info
+        items['article_body'] = article_body_str
+        items['publication_date'] = publication_date
+        items['publication_time'] = publication_time
+        items['created_at'] = created_at
+        items['image'] = image
+        items['reactions'] = reactions
+        items['author'] = author
+        items['doctype'] = doctype
+        items['url'] = url
+        items['tags'] = tags
+        items['twitter'] = twitter
+        items['facebook'] = facebook
+        items['iframe'] = iframe
+
+
+
+        yield items
+
+class DatabasePipeline(object):
+
+    def __init__(self, db, user, passwd, host):
+        self.db = db
+        self.user = user
+        self.passwd = passwd
+        self.host = host
+
+    def process_item(self, item, spider):
+        return item
