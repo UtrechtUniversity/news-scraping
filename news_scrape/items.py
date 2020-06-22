c9b2ee11397adce1c7f628cf4050334014f0d107
Shiva Nadi <s.nadi@uu.nl>
2020-06-22 15:40:57 +0200

    article_id is added to item

--- news_scrape/items.py
+++ news_scrape/items.py
@@ -11,6 +11,7 @@ import scrapy
 class NewsScrapeItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    article_id = scrapy.Field()
     title = scrapy.Field()
     article_info = scrapy.Field()
     article_body = scrapy.Field()
