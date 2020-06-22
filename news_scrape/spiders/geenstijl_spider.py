7da33b97d8ce3dad295b1adfa83543eada67728c
Shiva Nadi <s.nadi@uu.nl>
2020-06-22 15:39:30 +0200

    1- article_id is added, 2- store list of all the images in a dict

--- news_scrape/spiders/geenstijl_spider.py
+++ news_scrape/spiders/geenstijl_spider.py
@@ -2,10 +2,10 @@ import scrapy
 import re
 import datetime
 from ..items import NewsScrapeItem
-import logging
+# import logging
+
 
 # logging.basicConfig(filename='geenstij.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
-
 class GeenstijSpider(scrapy.Spider):
     name = 'geenstijl'
 
@@ -13,11 +13,11 @@ class GeenstijSpider(scrapy.Spider):
 
     # function to remove html tags, used for cleaning footer
     TAG_RE = re.compile(r'<[^>]+>')
+
     def remove_tags(self, text):
         return self.TAG_RE.sub('', text)
 
    # function for removing extra \n and spaces, used in cleaning footer
-
     def clean(self, line):
         line = line.strip()
         line = re.sub("\n", "", line)
@@ -26,12 +26,12 @@ class GeenstijSpider(scrapy.Spider):
         line = re.sub(",", "", line)
         return line
 
-    # function for extracting time and date from footer
-
+    # function for extracting date from the footer
     def date_func(self, text):
         date = re.findall(r'[0-9]{2}[-|\/]{1}[0-9]{2}[-|\/]{1}[0-9]{2}', text)
         return date
 
+    # function for extracting time from the footer
     def time_func(self, text):
         time = re.findall(r'(?:[01]\d|2[0123]):(?:[012345]\d)', text)
         return time
@@ -47,58 +47,63 @@ class GeenstijSpider(scrapy.Spider):
         # if next_page is not None:
         #     yield response.follow(next_page, callback=self.parse)
 
-
     def parse_article(self, response):
         # def extract_with_xpath(query):
         #     return response.xpath(query).get()
-            # return response.xpath(query).get(default='').strip()
+        # return response.xpath(query).get(default='').strip()
+
+        article_id = response.xpath("//div[@class='main_content col-xs-12 col-sm-7']/article/@id").get()
 
         title = response.xpath("//div[@class='col-xs-12']/h1/text()").get()
-
 
         article_info = response.xpath("//div[@class='article-intro']/p/text()").get()
 
         article_body = response.xpath("//div[@class='article_content']/p//text()").getall()
         article_body_str = ''.join(str(e) for e in article_body)
 
-        footer= self.remove_tags(response.xpath("//div[@class='art-footer']/div[@class='col-xs-12 col-sm-7']").get())
+        footer = self.remove_tags(response.xpath("//div[@class='art-footer']/div[@class='col-xs-12 col-sm-7']").get())
         footer_clean = self.clean(footer)
         date = self.date_func(footer_clean)
         publication_date = ''.join(str(e) for e in date)
 
         time = self.time_func(footer_clean)
-        publication_time= ''.join(str(e) for e in time)
-
+        publication_time = ''.join(str(e) for e in time)
 
         created_at = datetime.datetime.now()
 
         image = response.xpath("//div[@class='article_content']/*/img/@src").getall()
-        if len(image) == 0:
-            image = 'No image'
+        if len(image) != 0:
+            image_dict = {i: image[i] for i in range(0, len(image))}
+            image_dict = str(image_dict)
+        else:
+            image_dict = 'No image'
+        #
+        # if len(image) == 0:
+        #     image = 'None'
 
         reactions = response.xpath("//div[@class='col-xs-12 col-sm-7']/a[@id='comment-count']/text()").get()
 
         author = response.xpath("//div[@class='col-xs-12 col-sm-7']/a[1]/text()").get()
 
-
         doctype = 'geenstijl.nl'
         url = response.url
 
         tags_list = response.xpath("//ul[@class='art-tags']/li/a/text()").getall()
-        tags =', '.join(str(i) for i in tags_list)
+        tags = ', '.join(str(i) for i in tags_list)
 
         twitter = response.xpath("//div[@class='icon-twitter pull-left']/a/@href").get()
         facebook = response.xpath("//div[@class='icon-facebook pull-left']/a/@href").get()
         iframe = response.xpath("//iframe[@class='puu-video_frame']/@data-src").get()
         #
         items = NewsScrapeItem()
+        items['article_id'] = article_id
         items['title'] = title
         items['article_info'] = article_info
         items['article_body'] = article_body_str
         items['publication_date'] = publication_date
         items['publication_time'] = publication_time
         items['created_at'] = created_at
-        items['image'] = image
+        items['image'] = image_dict
         items['reactions'] = reactions
         items['author'] = author
         items['doctype'] = doctype
@@ -110,6 +115,7 @@ class GeenstijSpider(scrapy.Spider):
 
         yield items
 
+
 class DatabasePipeline(object):
 
     def __init__(self, db, user, passwd, host):
@@ -120,4 +126,3 @@ class DatabasePipeline(object):
 
     def process_item(self, item, spider):
         return item
-
